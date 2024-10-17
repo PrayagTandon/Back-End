b9543db136d2b5d8e2f94b2241b63a8dc5386380
Prayag <tandonprayag99@gmail.com>
2024-10-17 19:02:43 -0400

    ...

---
+++ Lab-04/mongoose-mongodb-tutorial/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// Defining the User Schema
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+});
+
+// Creating the user model
+const User = mongoose.model('User', userSchema);
+
+module.exports(User);
