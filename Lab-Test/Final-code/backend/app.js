bb8178495451e58043ca67b9790a2e3ea81a0947
Prayag <tandonprayag99@gmail.com>
2024-10-17 21:04:44 -0400

    Added Backend to frontend

--- Lab-Test/Final-code/backend/app.js
+++ Lab-Test/Final-code/backend/app.js
@@ -1,8 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const comments = require('./routes/comments');
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+const app = express();
+const PORT = 3000;
 
-app.listen(3000)
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/socialMedia')
+    .then(() => console.log('Database Connected Successfully!!'))
+    .catch((err) => console.error(`Failed to connect to the Database: ${err}`))
+
+app.use('/api/comments', comments);
+
+app.listen(PORT, () => {
+    console.log(`Server is running at Port: ${PORT}`);
+});
