83614bb6cb56d4a7c578c88e139e46f03ff870d7
Prayag <tandonprayag99@gmail.com>
2024-10-22 10:40:36 -0400

    Server.js completed

--- Lab-05/socket-io-tutorial/servers/server.js
+++ Lab-05/socket-io-tutorial/servers/server.js
@@ -1,13 +1,31 @@
+const { Socket } = require('dgram');
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const socketIo = require('socket.io');
+
 
 const app = express();
 const server = http.createServer(app);
+const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 const port = 3000;
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
+});
+
+// Socket IO Codes
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log(`Message received: ${msg}`);
+        io.emit(`Chat message: ${msg}`); // This will broadcast the message to all connected clients
+    });
 });
